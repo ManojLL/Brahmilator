00ba4192514defae815080d84f82c5b5a683db8d
ManojLL <manojlakshan421@gmail.com>
2020-12-11 21:55:13 +0530

    add: homepage to app

---
+++ mobile_frontend/src/components/home/Home.js
@@ -0,0 +1,25 @@
+{
+    /**
+     * developer : Manoj Lakshan
+     * Date : 11/12/2020
+     * description : mobile app home component
+     * */
+}
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>HOME PAGE</Text>
+            </View>
+        );
+    }
+}
+
+export default Home;
