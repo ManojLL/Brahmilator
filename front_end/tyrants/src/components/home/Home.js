1a56c2cc0831d7d8e068e9225ce1e122f149f307
ManojLL <manojlakshan421@gmail.com>
2021-01-10 20:40:17 +0530

    add button to home page

--- front_end/tyrants/src/components/home/Home.js
+++ front_end/tyrants/src/components/home/Home.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    ImageBackground,
+    TouchableOpacity,
+    Button,
+} from 'react-native';
 
 class Home extends Component {
     constructor(props) {
@@ -10,7 +17,10 @@ class Home extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.imageBg}>
+                <View style={{
+                    justifyContent: 'center',
+                    alignItems: 'center', marginTop: 5,
+                }}>
                     <ImageBackground
                         source={require('../../images/icons/homeImge1.png')}
                         style={styles.img}/>
@@ -18,7 +28,7 @@ class Home extends Component {
                 <View style={styles.textContainer}>
                     <Text style={styles.text}>Welcome to</Text>
                     <View>
-                        <Text><Text style={styles.specText}>Brahimilator , </Text><Text
+                        <Text><Text style={styles.specText}>Brahimilator  , </Text><Text
                             style={styles.text}> mobile </Text></Text>
 
                     </View>
@@ -27,7 +37,17 @@ class Home extends Component {
                     <Text>Real time brahmi translator</Text>
                     <Text>at your service</Text>
                 </View>
+                <View style={styles.centerItems}>
+                    <ImageBackground
+                        source={require('../../images/backgroundImages/homeImg.png')}
+                        style={{width: 300, height: 300, marginTop: 10}}/>
+                </View>
 
+                <View style={[styles.centerItems]}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text>{'Get started'}</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -35,11 +55,10 @@ class Home extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#E5E5E5',
+        backgroundColor: '#333',
         height: '100%',
     },
-    imageBg: {
-        marginTop: 10,
+    centerItems: {
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -48,23 +67,34 @@ const styles = StyleSheet.create({
         height: 120,
     },
     textContainer: {
-        marginTop: 30,
+        marginTop: 20,
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 24,
     },
     text: {
-        fontSize: 24,
-        color: '#ffffff',
+        fontSize: 30,
+        color: '#fff',
+        fontWeight: 'bold',
     },
     specText: {
-        fontSize: 24,
+        fontSize: 30,
         color: '#FFC542',
+        fontWeight: 'bold',
     },
     subTitle: {
-        marginTop:20,
+        marginTop: 20,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    button: {
+        marginTop: 40,
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 60,
+        paddingRight: 60,
+        backgroundColor: '#FFC542',
+        borderRadius:15,
     },
 
 });
