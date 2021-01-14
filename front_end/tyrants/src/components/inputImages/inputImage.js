a37d5fcc9043df522e902241d3402442a8601f48
ManojLL <manojlakshan421@gmail.com>
2021-01-14 14:07:32 +0530

    add camera

---
+++ front_end/tyrants/src/components/inputImages/inputImage.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    ImageBackground,
+    TouchableOpacity,
+    Button,
+} from 'react-native';
+import { RNCamera } from 'react-native-camera'
+
+class InputImg extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                <RNCamera
+                    style={{ flex: 1, alignItems: 'center' }}
+                    ref={ref => {
+                        this.camera = ref
+                    }}
+                />
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#333',
+        height: '100%',
+    },
+    centerItems: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+
+
+});
+export default InputImg;
