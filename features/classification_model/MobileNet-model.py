61b3ccad826f2f3affaafd9b1a0d31ed99d1c6ae
malindumn <61543091+malindumn@users.noreply.github.com>
2021-02-04 14:02:51 +0530

    added 3 models

--- features/classification_model/MobileNet-model.py
+++ features/classification_model/MobileNet-model.py
@@ -4,13 +4,16 @@ from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.optimizer_v2.adam import Adam
 
-train_path = "Early_Brahmi/train"
+train_path = "Early_Brahmi/train"     # 66 classes
+train_path1 = "Early_Brahmi/train 1"  # all classes (with and without data) - 71
+train_path2 = "Early_Brahmi/train 2"  # classes with more than 30 images - 8
+train_path3 = "Early_Brahmi/train 3"  # classes with data - 54
 test_path = "Early_Brahmi/test"
 valid_path = "Early_Brahmi/validation"
 
 train_batches = ImageDataGenerator(
     preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
-    directory=train_path, target_size=(224, 224), batch_size=10)
+    directory=train_path, target_size=(224, 224), batch_size=10, shuffle=True)
 valid_batches = ImageDataGenerator(
     preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
     directory=valid_path, target_size=(224, 224), batch_size=10)
@@ -23,7 +26,7 @@ mobile.summary()
 
 x = mobile.layers[-6].output
 
-output = Dense(units=71, activation='softmax')(x)
+output = Dense(units=66, activation='softmax')(x)
 
 model = Model(inputs=mobile.input, outputs=output)
 
@@ -36,7 +39,7 @@ model.fit(x=train_batches,
           steps_per_epoch=len(train_batches),
           validation_data=valid_batches,
           validation_steps=len(valid_batches),
-          epochs=30,
+          epochs=150,
           verbose=2
           )
 
