d88625791f5b1f20a16204b42c5657ec5d93d91e
ManojLL <manojlakshan421@gmail.com>
2021-03-03 08:55:23 +0530

    update image preview screeen

--- front_end/tyrants/src/components/inputImages/imagePreview.js
+++ front_end/tyrants/src/components/inputImages/imagePreview.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     View,
     Text,
     StyleSheet,
     ImageBackground,
     TouchableOpacity,
-    Button,
+    Button,Image
 } from 'react-native';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 
 class ImagePreview extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            ImageUri: '',
+        };
     }
 
     render() {
@@ -21,30 +23,31 @@ class ImagePreview extends Component {
             <View style={styles.container}>
 
                 <View style={[styles.imagePrev, styles.centerItems]}>
-                    <ImageBackground
-                        source={require('../../images/backgroundImages/inc.png')}
-                        style={{ width: wp('90%'), height: hp("70%") }} />
+                    <Image
+                        source={{uri:this.props.route.params.imgUri}}
+                        // source={require(this.props.navigation.state.params.imgUri)}
+                        style={{width: wp('90%'), height: hp("70%")}}/>
                 </View>
-                <View style={[styles.toolBar,styles.centerItems]}>
-                    <View style={{ flex: 1, flexDirection: 'row' }}>
-                    <TouchableOpacity activeOpacity={0.5}
-                                    onPress={() => this.props.navigation.navigate('Camera')}>
-                    <ImageBackground
-                        source={require('../../images/icons/retake.png')}
-                        style={{ width:   wp('7%'), height: hp('3%'),marginLeft:20  }} />
-                       </TouchableOpacity>
+                <View style={[styles.toolBar, styles.centerItems]}>
+                    <View style={{flex: 1, flexDirection: 'row'}}>
+                        <TouchableOpacity activeOpacity={0.5}
+                                          onPress={() => this.props.navigation.navigate('Camera')}>
+                            <ImageBackground
+                                source={require('../../images/icons/retake.png')}
+                                style={{width: wp('7%'), height: hp('3%'), marginLeft: 20}}/>
+                        </TouchableOpacity>
                     </View>
-                    <View style={{ flex: 1, flexDirection: 'row-reverse' }}>
-                    <TouchableOpacity onPress={() => this.props.navigation.push('Pre-process')}>
-                    <ImageBackground
-                        source={require('../../images/icons/process.png')}
-                        style={{width:   wp('7%'), height: hp('3%'),marginRight:20 }} />
+                    <View style={{flex: 1, flexDirection: 'row-reverse'}}>
+                        <TouchableOpacity onPress={() => this.props.navigation.push('Pre-process',{imgUri:this.props.route.params.imgUri})}>
+                            <ImageBackground
+                                source={require('../../images/icons/process.png')}
+                                style={{width: wp('7%'), height: hp('3%'), marginRight: 20}}/>
                         </TouchableOpacity>
                         <TouchableOpacity>
-                    <ImageBackground
-                        source={require('../../images/icons/save.png')}
-                        style={{width:  wp('7%'), height: hp('3%'),marginRight:20 }} />
-                                        </TouchableOpacity>
+                            <ImageBackground
+                                source={require('../../images/icons/save.png')}
+                                style={{width: wp('7%'), height: hp('3%'), marginRight: 20}}/>
+                        </TouchableOpacity>
                     </View>
 
                 </View>
@@ -69,7 +72,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#000',
         height: '15%',
         flex: 1, flexDirection: 'row',
-        padding:10,
+        padding: 10,
     }
 
 
