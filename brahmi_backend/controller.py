1f9eeb3a23d70378d21337e98db5556532e6df3c
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2021-03-18 12:50:22 +0530

    path updated

--- brahmi_backend/controller.py
+++ brahmi_backend/controller.py
@@ -6,19 +6,19 @@ from util import make_response
 
 app = Flask(__name__)
 
-data_save_path = "D:/Studies/IIT/Level 5/2 Sem/SDGP/SDGP_THE_Tyrants/brahmi_backend/data"
+input_data = "input_data"
 
 @app.route("/api/getLetters", methods=["POST"])
 def translateLetters():
     try:
         image = request.files["image"]
         image_name = image.filename
-        image.save(os.path.join(data_save_path, image_name))
-        if filetype.is_image(os.path.join(data_save_path, image_name)):
+        image.save(os.path.join(input_data, image_name))
+        if filetype.is_image(os.path.join(input_data, image_name)):
             classify_letters = services.classify_letters()
             result = {'letter': classify_letters}
             response = make_response(result,True,200)
-            os.remove(os.path.join(data_save_path, image_name))
+            os.remove(os.path.join(input_data, image_name))
             return Response(response=response, status=200, mimetype='application/json')
         else:
             response = make_response('The file is NOT an Image', False, 200)
@@ -29,8 +29,9 @@ def translateLetters():
         return Response(response=response, status=404, mimetype='application/json')
 
 
-# @app.route("/api/getWords", methods=["POST"])
-# def getWords():
+# @app.route("/api/preProcess", methods=["POST"])
+# def preProcessImage():
+#     print("todo")
 
 if __name__ == '__main__':
     app.run()
