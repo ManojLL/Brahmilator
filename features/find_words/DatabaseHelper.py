fe0da3b821a8a997008813e2afb925fe667515e2
NimendraK <nimendra.2019264@iit.ac.lk>
2021-04-09 09:20:26 +0530

    csv reader added

--- features/find_words/DatabaseHelper.py
+++ features/find_words/DatabaseHelper.py
@@ -1,4 +1,5 @@
 import pymongo
+import csv
 
 myclient = pymongo.MongoClient(
     "mongodb+srv://brahmilator_db:brahmilator123@cluster0.zf5dm.mongodb.net/brahmilator_db?retryWrites=true&w=majority")
@@ -14,10 +15,22 @@ if "brahmilator_database" in dblist:
 
 column = mydb["words"]
 
-words = [
-    {"brahmi": "akara", "eng": "house"},
-    {"brahmi": "biku", "eng": "monk"},
-]
+
+# words = [
+#     {"brahmi": "akara", "eng": "house"},
+#     {"brahmi": "biku", "eng": "monk"},
+# ]
+
+def read_csv(filename):
+    with open(filename) as f:
+        file_data = csv.reader(f)
+        headers = next(file_data)
+        return [dict(zip(headers, i)) for i in file_data]
+
+
+words = read_csv("db1.txt")
+
+print(words)
 
 # insert data into the column one -> word
 x = column.insert_many(words)
@@ -25,7 +38,7 @@ x = column.insert_many(words)
 # print list of the _id values of the inserted documents:
 print(x.inserted_ids)
 
-myquery = {"brahmi": "biku"}
+myquery = {"id": "1"}
 
 mydoc = column.find(myquery)
 
