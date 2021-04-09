02c9383bc259adcfdeecbbda77bffd94f4056a42
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2021-04-09 21:22:44 +0530

    possible word finder modified

--- features/find_words/dbtest.py
+++ features/find_words/dbtest.py
@@ -1,18 +1,22 @@
 import pymongo
 import csv
 
-def search(query):
-    # print('db test')
-    print(query)
+def search(value):
+    myquery = {"word": value}
+
     myclient = pymongo.MongoClient(
         "mongodb+srv://brahmilator_db:brahmilator123@cluster0.zf5dm.mongodb.net/brahmilator_db?retryWrites=true&w=majority")
 
     mydb = myclient["brahmilator_database"]
 
+
     column = mydb["words"]
 
-    mydoc = column.find(query)
+    mydoc = column.find(myquery)
+
+    possible_meanings = []
 
     for x in mydoc:
-        print("response")
-        print(x)
+        possible_meanings.append(x['meaning'])
+
+    return possible_meanings
