ad49592e56147dff90e485cf210149e90f23de33
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2021-04-10 01:29:47 +0530

    word finder updated

--- features/find_words/possible_word_finder.py
+++ features/find_words/possible_word_finder.py
@@ -1,4 +1,5 @@
 from math import pow
+import pymongo
 from dbtest import search
 
 result = {}
@@ -25,20 +26,34 @@ def subsequences(str):
         x = x.split()
         for y in x:
             posibilities.append(y)
-            
+
     return posibilities
+
+myclient = pymongo.MongoClient(
+        "mongodb+srv://brahmilator_db:brahmilator123@cluster0.zf5dm.mongodb.net/brahmilator_db?retryWrites=true&w=majority")
+
+mydb = myclient["brahmilator_database"]
+
+column = mydb["words"]
 
 str = ['ba', 'ta', 'na', 'ga']
 posibile_words = subsequences(str)
 
+print('all posibilities')
+print(*posibile_words, sep=" , ")
+
 posibile_words = dict.fromkeys(posibile_words)
 
+print('\nafter remove duplicates')
+print(*posibile_words, sep=" , ")
+
 for x in posibile_words:
-    print(x)
-    possible_meanings = search(x)
+    possible_meanings = search(x, column)
     if len(possible_meanings) != 0:
         result[x] = possible_meanings
 
+print('\nmeanings')
 for keys, values in result.items():
     print(keys)
     print(values)
+    print()
