5869d27cf39dab8c170cdfef84d109a9a4c77796
ManojLL <manojlakshan421@gmail.com>
2021-04-12 15:54:38 +0530

    add words to result screen

--- front_end/tyrants/src/components/menuAndOutPut/result.js
+++ front_end/tyrants/src/components/menuAndOutPut/result.js
@@ -14,19 +14,22 @@ import {Dropdown} from "react-native-material-dropdown";
 import Textarea from "react-native-textarea";
 
 import BottomNavigator from "../navigators/BottomNavigator";
+import index from "@react-native-community/netinfo/src/index";
 
 class Result extends Component {
     constructor(props) {
         super(props);
         this.state = {
             currentLan: 'en',
-            findWords: {}
+            findWord: this.props.route.params.findWords,
+            wordWithMeaning:this.props.route.params.withMeaning,
+
         };
     }
 
     componentDidMount() {
-        this.setState({findWords: this.props.route.params.findWords})
-        console.log(this.state.findWords)
+
+        console.log(this.state.findWord)
     }
 
 
@@ -86,22 +89,14 @@ class Result extends Component {
                 </View>
 
                 <ScrollView showsVerticalScrollIndicator={false}>
-                    {/* Translated inscription in Sinhala  */}
-                    <Textarea
-                        containerStyle={styles.textareaContainer}
-                        style={styles.textarea}
-                        onChangeText={this.onChange}
-                        defaultValue={this.state.text}
-                        editable={false}
-                        maxLength={1000}
-                        placeholder={
-                            "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,"
-                        }
-                        placeholderTextColor={"#c7c7c7"}
-                        underlineColorAndroid={"transparent"}
-                    />
+
                     <View>
-                        {
+                        {this.state.findWord.map((w,index)=>(
+                            <View key={index}>
+                                <Text
+                                    style={{color: "#ffffff", marginLeft: 20, fontSize: hp('3%'),}}>{w}</Text>
+                            </View>
+                        ))
 
                         }
 
