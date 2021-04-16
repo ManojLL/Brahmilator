f093d5449d1f0194287148d9e2efc58621dfec90
malindumn <61543091+malindumn@users.noreply.github.com>
2021-04-17 00:19:18 +0530

    classification updated

--- features/classification_model/MobileNet-model.py
+++ features/classification_model/MobileNet-model.py
@@ -14,19 +14,19 @@ valid_path = "brahmi_data_updated/val"
 train_batches = ImageDataGenerator(
     preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
     directory=train_path, target_size=(224, 224), batch_size=10, shuffle=True)
-valid_batches = ImageDataGenerator(
-    preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
-    directory=valid_path, target_size=(224, 224), batch_size=10)
-test_batches = ImageDataGenerator(
-    preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
-    directory=test_path, target_size=(224, 224), batch_size=10, shuffle=False)
+# valid_batches = ImageDataGenerator(
+#     preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+#     directory=valid_path, target_size=(224, 224), batch_size=10)
+# test_batches = ImageDataGenerator(
+#     preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+#     directory=test_path, target_size=(224, 224), batch_size=10, shuffle=False)
 
 mobile = tf.keras.applications.mobilenet.MobileNet()
 mobile.summary()
 
 x = mobile.layers[-6].output
 
-output = Dense(units=15, activation='softmax')(x)
+output = Dense(units=13, activation='softmax')(x)
 
 model = Model(inputs=mobile.input, outputs=output)
 
@@ -37,12 +37,12 @@ model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metric
 
 model.fit(x=train_batches,
           steps_per_epoch=len(train_batches),
-          validation_data=valid_batches,
-          validation_steps=len(valid_batches),
+          # validation_data=valid_batches,
+          # validation_steps=len(valid_batches),
           epochs=50,
           verbose=2
           )
 
-model_save_path = "./saved-models/saved-models-mobilenet"
+model_save_path = "./saved-models/model"
 
 tf.saved_model.save(model, model_save_path)
