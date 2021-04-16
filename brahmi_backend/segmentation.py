1ba08eb50f0c02a4b199b2306d138daee6713361
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2021-04-16 11:34:50 +0530

    change segmentaion module version using in API

--- brahmi_backend/segmentation.py
+++ brahmi_backend/segmentation.py
@@ -1,24 +1,9 @@
-import os
-import shutil
 import sys
-
 import cv2
 import numpy as np
 
-input_data = "input_data"
-
 sys.setrecursionlimit(10 ** 6)
 
-folder = 'segmented_letters'
-for filename in os.listdir(folder):
-    file_path = os.path.join(folder, filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 def line_array(array):
     list_x_upper = []
@@ -126,147 +111,124 @@ def end_wrd_dtct(lines, i, bin_img, mean_lttr_width, width, final_thr):
         final_thr[lines[i][0]:lines[i][1], x] = 255
     return endlines
 
+def image_segmentation():
+    try:
+        src_img = cv2.imread('input_data/plate.png')
 
-def letter_seg(lines_img, x_lines, i):
-    copy_img = lines_img[i].copy()
-    x_linescopy = x_lines[i].copy()
+        copy = src_img.copy()
+        height = src_img.shape[0]
+        width = src_img.shape[1]
 
-    letter_k = []
+        src_img_copy = src_img.copy()
 
-    contours, hierarchy = cv2.findContours(copy_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        if cv2.contourArea(cnt) > 50:
-            x, y, w, h = cv2.boundingRect(cnt)
-            # letter_img.append(lines_img[i][y:y+h, x:x+w])
-            letter_k.append((x, y, w, h))
+        src_img = cv2.resize(copy, dsize=(1320, int(1320 * height / width)), interpolation=cv2.INTER_AREA)
 
-    letter = sorted(letter_k, key=lambda student: student[0])
+        height = src_img.shape[0]
+        width = src_img.shape[1]
 
-    word = 1
-    letter_index = 0
-    for e in range(len(letter)):
-        if (letter[e][0] < x_linescopy[0]):
-            letter_index += 1
-            letter_img_tmp = lines_img[i][letter[e][1] - 5:letter[e][1] + letter[e][3] + 5,
-                             letter[e][0] - 5:letter[e][0] + letter[e][2] + 5]
-            try:
-                letter_img = cv2.resize(letter_img_tmp, dsize=(224, 224), interpolation=cv2.INTER_LINEAR)
-            except Exception as e:
-                print(str(e))
-            cv2.imwrite(
-                'segmented_letters/segmented_img' + str(i + 1) + '_' + str(word) + '_' + str(letter_index) + '.jpg',
-                255 - letter_img)
+        grey_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
+
+        bin_img = cv2.adaptiveThreshold(grey_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 20)
+        bin_img1 = bin_img.copy()
+        bin_img2 = bin_img.copy()
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        kernel1 = np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]], dtype=np.uint8)
+
+        final_thr = cv2.morphologyEx(bin_img, cv2.MORPH_CLOSE, kernel)
+        contr_retrival = final_thr.copy()
+
+
+        count_x = np.zeros(shape=(height))
+        for y in range(height):
+            for x in range(width):
+                if bin_img[y][x] == 255:
+                    count_x[y] = count_x[y] + 1
+
+        upper_lines, lower_lines = line_array(count_x)
+
+        upperlines, lowerlines = refine_array(upper_lines, lower_lines)
+
+        if len(upperlines) == len(lowerlines):
+            lines = []
+            for y in upperlines:
+                final_thr[y][:] = 255
+            for y in lowerlines:
+                final_thr[y][:] = 255
+            for y in range(len(upperlines)):
+                lines.append((upperlines[y], lowerlines[y]))
+
         else:
-            x_linescopy.pop(0)
-            word += 1
-            letter_index = 1
-            letter_img_tmp = lines_img[i][letter[e][1] - 5:letter[e][1] + letter[e][3] + 5,
-                             letter[e][0] - 5:letter[e][0] + letter[e][2] + 5]
-            try:
-                letter_img = cv2.resize(letter_img_tmp, dsize=(224, 224), interpolation=cv2.INTER_LINEAR)
-            except Exception as e:
-                print(str(e))
-            cv2.imwrite(
-                'segmented_letters/segmented_img' + str(i + 1) + '_' + str(word) + '_' + str(letter_index) + '.jpg',
-                255 - letter_img)
+            k = cv2.waitKey(0)
+            while 1:
+                k = cv2.waitKey(0)
+                if k & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    exit()
+
+        lines = np.array(lines)
+
+        no_of_lines = len(lines)
+
+        lines_img = []
+
+        for i in range(no_of_lines):
+            lines_img.append(bin_img2[lines[i][0]:lines[i][1], :])
 
 
-# ------------------/Functions-----------------#
+        contours, hierarchy = cv2.findContours(contr_retrival, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        final_contr = np.zeros((final_thr.shape[0], final_thr.shape[1], 3), dtype=np.uint8)
+        cv2.drawContours(src_img, contours, -1, (0, 255, 0), 1)
+
+        mean_lttr_width = letter_width(contours)
+
+        x_lines = []
+
+        for i in range(len(lines_img)):
+            x_lines.append(end_wrd_dtct(lines, i, bin_img, mean_lttr_width, width, final_thr))
+
+        for i in range(len(x_lines)):
+            x_lines[i].append(width)
+
+        chr_img = bin_img1.copy()
+
+        contours, hierarchy = cv2.findContours(chr_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        new_height = src_img.shape[0]
+        new_width = src_img.shape[1]
+
+        resized_image = cv2.resize(src_img_copy, (new_width, new_height))
+
+        edges = []
+
+        for cnt in contours:
+            if cv2.contourArea(cnt) > 20:
+                x, y, w, h = cv2.boundingRect(cnt)
+                temp = []
+                temp.append(x)
+                temp.append(y)
+                temp.append(w)
+                temp.append(h)
+                edges.append(temp)
+                cv2.rectangle(src_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
-# -------------Thresholding Image--------------#
+        n = len(edges)
 
-def image_segmentation(img):
-    test_path = os.path.join(input_data)
-    src_img = cv2.imread(os.path.join(test_path, img))
-    copy = src_img.copy()
-    height = src_img.shape[0]
-    width = src_img.shape[1]
+        for i in range(n-1):
+            for j in range(0, n - i - 1):
+                if edges[j][0] > edges[j + 1][0]:
+                    edges[j], edges[j + 1] = edges[j + 1], edges[j]
 
-    src_img = cv2.resize(copy, dsize=(1320, int(1320 * height / width)), interpolation=cv2.INTER_AREA)
+        i = 0
+        for edge in edges:
+            crop = resized_image[edge[1]:(edge[1] + edge[3]), edge[0]:(edge[0] + edge[2])]
+            # crop = cv2.resize(crop, (224, 224))
+            cv2.imwrite("segmented_letters/crop_{0}.png".format(i), crop)
+            i = i + 1
 
-    height = src_img.shape[0]
-    width = src_img.shape[1]
+        return True
 
-    grey_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
-
-    bin_img = cv2.adaptiveThreshold(grey_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 20)
-    bin_img1 = bin_img.copy()
-    bin_img2 = bin_img.copy()
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    kernel1 = np.array([[1, 0, 1], [0, 1, 0], [1, 0, 1]], dtype=np.uint8)
-    final_thr = cv2.morphologyEx(bin_img, cv2.MORPH_CLOSE, kernel)
-    contr_retrival = final_thr.copy()
-
-    # -------------/Thresholding Image-------------#
-
-    # -------------Line Detection------------------#
-    count_x = np.zeros(shape=(height))
-    for y in range(height):
-        for x in range(width):
-            if bin_img[y][x] == 255:
-                count_x[y] = count_x[y] + 1
-
-    upper_lines, lower_lines = line_array(count_x)
-
-    upperlines, lowerlines = refine_array(upper_lines, lower_lines)
-
-    if len(upperlines) == len(lowerlines):
-        lines = []
-        for y in upperlines:
-            final_thr[y][:] = 255
-        for y in lowerlines:
-            final_thr[y][:] = 255
-        for y in range(len(upperlines)):
-            lines.append((upperlines[y], lowerlines[y]))
-
-    else:
+    except:
+        print('error in segmentation')
         return False
-
-    lines = np.array(lines)
-
-    no_of_lines = len(lines)
-
-    lines_img = []
-
-    for i in range(no_of_lines):
-        lines_img.append(bin_img2[lines[i][0]:lines[i][1], :])
-
-    # -------------/Line Detection-----------------#
-
-    # -------------Letter Width Calculation--------#
-
-    contours, hierarchy = cv2.findContours(contr_retrival, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    final_contr = np.zeros((final_thr.shape[0], final_thr.shape[1], 3), dtype=np.uint8)
-    cv2.drawContours(src_img, contours, -1, (0, 255, 0), 1)
-
-    mean_lttr_width = letter_width(contours)
-
-    x_lines = []
-
-    for i in range(len(lines_img)):
-        x_lines.append(end_wrd_dtct(lines, i, bin_img, mean_lttr_width, width, final_thr))
-
-    for i in range(len(x_lines)):
-        x_lines[i].append(width)
-
-    for i in range(len(lines)):
-        letter_seg(lines_img, x_lines, i)
-
-    # ------------\Letter Segmentation-------------#
-
-    # -------------Character segmenting------------#
-
-    chr_img = bin_img1.copy()
-
-    contours, hierarchy = cv2.findContours(chr_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    for cnt in contours:
-        if cv2.contourArea(cnt) > 20:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(src_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # -------------/Character segmenting-----------#
-
-    return True
