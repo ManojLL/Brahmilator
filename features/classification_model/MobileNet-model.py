76dbe6b685f7c55841d6234132442eeb86e1bcd2
malindumn <61543091+malindumn@users.noreply.github.com>
2021-04-18 12:51:37 +0530

    plots added

--- features/classification_model/MobileNet-model.py
+++ features/classification_model/MobileNet-model.py
@@ -3,6 +3,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.optimizer_v2.adam import Adam
+import matplotlib.pyplot as plt
 
 train_path = "data_1/train"
 test_path = "data_1/test"
@@ -23,7 +24,7 @@ mobile.summary()
 
 x = mobile.layers[-6].output
 
-output = Dense(units=13, activation='softmax')(x)
+output = Dense(units=15, activation='softmax')(x)
 
 model = Model(inputs=mobile.input, outputs=output)
 
@@ -32,14 +33,34 @@ for layer in model.layers[:-23]:
 
 model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x=train_batches,
-          steps_per_epoch=len(train_batches),
-          validation_data=valid_batches,
-          validation_steps=len(valid_batches),
-          epochs=50,
-          verbose=2
-          )
+history = model.fit(x=train_batches,
+                    steps_per_epoch=len(train_batches),
+                    validation_data=valid_batches,
+                    validation_steps=len(valid_batches),
+                    epochs=10,
+                    verbose=2
+                    )
 
-model_save_path = "./saved-models/saved-models-mobilenet"
+loss_train = history.history['loss']
+loss_val = history.history['val_loss']
+plt.plot(loss_train, 'g', label='Training loss')
+plt.plot(loss_val, 'b', label='validation loss')
+plt.title('Training and Validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
 
-tf.saved_model.save(model, model_save_path)
+loss_train = history.history['accuracy']
+loss_val = history.history['val_accuracy']
+plt.plot(loss_train, 'g', label='Training accuracy')
+plt.plot(loss_val, 'b', label='validation accuracy')
+plt.title('Training and Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+# model_save_path = "./saved-models/saved-models-mobilenet"
+#
+# tf.saved_model.save(model, model_save_path)
