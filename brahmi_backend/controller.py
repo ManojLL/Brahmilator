b9d456e0942142b6d582ed333b2a9ff77dd0485d
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2021-04-18 10:23:11 +0530

    updated

--- brahmi_backend/controller.py
+++ brahmi_backend/controller.py
@@ -1,5 +1,4 @@
 import os
-import cv2
 from flask import Flask, request, Response
 import filetype
 from tqdm import tqdm
@@ -68,62 +67,11 @@ def translateLetters():
         return Response(response=response, status=404, mimetype='application/json')
 
 
-    # global image_name
-    # try:
-    #     image = request.files["image"]
-    #     image_name = image.filename
-    #     image.save(os.path.join(input_data, image_name))
-    #
-    #     if filetype.is_image(os.path.join(input_data, image_name)):
-    #         flag = image_segmentation(image_name)
-    #
-    #         if (flag == True):
-    #             result = {}
-    #             classify_letters = brahmi_classifier.classify_letters()
-    #             result['letter'] = classify_letters
-    #
-    #             test_path = os.path.join(segmented_letters)
-    #             segmented_images = []
-    #
-    #             for img in tqdm(os.listdir(test_path)):
-    #                 image_path = os.path.join(test_path, img)
-    #                 pil_img = Image.open(image_path, mode='r')
-    #                 byte_arr = io.BytesIO()
-    #                 pil_img.save(byte_arr, format='PNG')
-    #                 encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii')
-    #                 segmented_images.append(encoded_img)
-    #                 os.remove(os.path.join(test_path, img))
-    #
-    #             result['images'] = segmented_images
-    #
-    #             response = make_response(result, True, 200)
-    #             os.remove(os.path.join(input_data, image_name))
-    #             return Response(response=response, status=200, mimetype='application/json')
-    #         else:
-    #             test_path = os.path.join(segmented_letters)
-    #
-    #             for img in tqdm(os.listdir(test_path)):
-    #                 os.remove(os.path.join(test_path, img))
-    #
-    #             os.remove(os.path.join(input_data, image_name))
-    #             response = make_response("Too much noise in image", True, 200)
-    #             return Response(response=response, status=200, mimetype='application/json')
-    #     else:
-    #         os.remove(os.path.join(input_data, image_name))
-    #         response = make_response('The file is NOT an Image', False, 403)
-    #         return Response(response=response, status=403, mimetype='application/json')
-    # except Exception as e:
-    #     os.remove(os.path.join(input_data, image_name))
-    #     response = make_response('The file is NOT FOUND', False, 404)
-    #     return Response(response=response, status=404, mimetype='application/json')
-
-
 @app.route('/api/getPossibleWords', methods=['POST'])
 def getPossibleWords():
     try:
         data = request.get_json()['letters']
-        myclient = pymongo.MongoClient(
-            "mongodb+srv://brahmilator_db:brahmilator123@cluster0.zf5dm.mongodb.net/brahmilator_db?retryWrites=true&w=majority")
+        myclient = pymongo.MongoClient("mongodb+srv://brahmilator_db:brahmilator123@cluster0.zf5dm.mongodb.net/brahmilator_db?retryWrites=true&w=majority")
         mydb = myclient["brahmilator_database"]
         column = mydb["words"]
 
@@ -144,7 +92,6 @@ def getPossibleWords():
             response = make_response("Possible match not found", True, 404)
             return Response(response=response, status=200, mimetype='application/json')
     except Exception as e:
-        print(e)
         response = make_response('Something went wrong', False, 404)
         return Response(response=response, status=404, mimetype='application/json')
 
