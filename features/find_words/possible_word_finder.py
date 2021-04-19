80c5b58554494d305e7da00a17cfe384f6117946
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2021-04-19 16:34:22 +0530

    updated

--- features/find_words/possible_word_finder.py
+++ features/find_words/possible_word_finder.py
@@ -36,7 +36,7 @@ mydb = myclient["brahmilator_database"]
 
 column = mydb["words"]
 
-str = ['ba', 'ta', 'na', 'ga']
+str = ["ba", "ta", "gu", "ta",  "ha", "le", "ne", "sa", "ga", "sa"]
 posibile_words = subsequences(str)
 
 print('all posibilities')
@@ -47,13 +47,36 @@ posibile_words = dict.fromkeys(posibile_words)
 print('\nafter remove duplicates')
 print(*posibile_words, sep=" , ")
 
+given_letters = ''.join(str)
+
+word_list = []
+meaning_list = []
+
 for x in posibile_words:
     possible_meanings = search(x, column)
     if len(possible_meanings) != 0:
         result[x] = possible_meanings
+        word_list.append(x)
+        meaning_list.append(possible_meanings)
+
+index = []
+for key, value in result.items():
+    index.append(given_letters.find(key))
+
+n = len(index)
+for i in range(n - 1):
+    for j in range(0, n - i - 1):
+        if index[j] > index[j + 1]:
+            word_list[j], word_list[j + 1] = word_list[j + 1], word_list[j]
+            meaning_list[j], meaning_list[j + 1] = meaning_list[j + 1], meaning_list[j]
+            index[j], index[j + 1] = index[j + 1], index[j]
+
+final_result = {}
+for i in range(n):
+    final_result[word_list[i]] = meaning_list[i]
 
 print('\nmeanings')
-for keys, values in result.items():
+for keys, values in final_result.items():
     print(keys)
     print(values)
     print()
