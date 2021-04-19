c1232ab0e598e406272fc133fa04884828c827bc
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2021-04-19 21:25:22 +0530

    finalized features updated

--- front_end/tyrants/src/components/inputImages/dirStorage.js
+++
@@ -1,9 +0,0 @@
-import { Platform } from 'react-native';
-const RNFS = require('react-native-fs');
-
-export const dirHome = Platform.select({
-  android: `${RNFS.DocumentDirectoryPath}/tyrants`,
-  ios: `${RNFS.ExternalStorageDirectoryPath}/tyrants`,
-});
-
-export const dirPicutures = `${dirHome}/images`;
