a36c6ea65295650dddf4e6823a036d9af7082d61
Pubudu Mihiranga <i.pubudumihiranga@gmail.com>
2021-04-20 17:25:06 +0530

    pre-process-info screen modified

--- brahmi_frontend/tyrants/src/components/menuAndOutPut/preProcessInfo.js
+++ brahmi_frontend/tyrants/src/components/menuAndOutPut/preProcessInfo.js
@@ -15,6 +15,8 @@ import Close from '../../images/icons/close.svg';
 import Opening from '../../images/icons/opening.svg';
 import Closing from '../../images/icons/closing.svg';
 
+import {Col, Row, Grid} from 'react-native-easy-grid';
+
 LogBox.ignoreAllLogs();
 
 class Info extends Component {
@@ -58,55 +60,102 @@ class Info extends Component {
         <ScrollView>
           <Card style={styles.card}>
             <View style={{}}>
-              <Threshold />
-              <Text>
+            <Grid>
+              <Row>
+                <Col style={[styles.alignCenter], [styles.width=25]}>
+                <Threshold />
+                </Col>
+                <Col style={[styles.alignCenter], [styles.width=75]}>
+                <Text>
                 Reduces the photo to black and white; colors lighter than the
                 selectable threshold are converted to white,darker colors will
                 be black
               </Text>
+                </Col>
+              </Row>
+            </Grid>
+              
+              
             </View>
           </Card>
 
           <Card style={styles.card}>
             <View style={{}}>
-              <Erosion />
-
-              <Text>
+            <Grid>
+              <Row>
+                <Col style={[styles.alignCenter]}>
+                <Erosion />
+                </Col>
+                <Col style={styles.alignCenter}>
+                <Text>
                 It erodes away the boundaries of foreground object. It is useful
                 for removing smaill white noises detach two connected objects
               </Text>
+                </Col>
+              </Row>
+            </Grid>
+              
+
+              
             </View>
           </Card>
 
           <Card style={styles.card}>
             <View style={{}}>
-              <Dialation />
-
-              <Text>
+            <Grid>
+              <Row>
+                <Col style={[styles.alignCenter]}>
+                <Dialation />
+                </Col>
+                <Col style={styles.alignCenter}>
+                <Text>
                 It is just opposite of erosion. It is also useful in joining
                 broken parts of an object
               </Text>
+                </Col>
+              </Row>
+            </Grid>
+              
+
+              
             </View>
           </Card>
 
           <Card style={styles.card}>
             <View style={{}}>
-              <Opening />
-
-              <Text>
-                It is obtained by the erosion of an image followed by a dilation. Useful for removing small objects
+            <Grid>
+              <Row>
+                <Col style={[styles.alignCenter]}>
+                <Opening />
+                </Col>
+                <Col style={styles.alignCenter}>
+                  <Text>
+                  It is obtained by the erosion of an image followed by a dilation. Useful for removing small objects
                 (it is assumed that the objects are bright on a dark foreground)
-              </Text>
+                  </Text>
+                </Col>
+              </Row>
+            </Grid>
             </View>
           </Card>
 
           <Card style={styles.card}>
             <View style={{}}>
-              <Closing />
+            <Grid>
+              <Row>
+                <Col style={[styles.alignCenter]}>
+                  <Closing />
+                </Col>
+                <Col style={styles.alignCenter}>
+                  <Text>
+                    It is obtained by the dilation of an image followed by an erosion. Useful to remove small holes (dark regions)
+                  </Text>
+                </Col>
+              </Row>
+            </Grid>
+              
 
-              <Text>
-                It is obtained by the dilation of an image followed by an erosion. Useful to remove small holes (dark regions)
-              </Text>
+              
             </View>
           </Card>
         </ScrollView>
@@ -130,7 +179,7 @@ const styles = StyleSheet.create({
     width: 342,
     padding: 10,
     margin: 20,
-    // marginBottom: 10
+    marginBottom: 2
   },
   textContainer: {
     marginTop: 20,
