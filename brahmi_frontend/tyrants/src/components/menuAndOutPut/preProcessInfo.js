d09aeae69c6ace63ae17245bfc4f5631a9122be0
NimendraK <nimendra.2019264@iit.ac.lk>
2021-04-22 01:27:46 +0530

    validate model configured

--- brahmi_frontend/tyrants/src/components/menuAndOutPut/preProcessInfo.js
+++ brahmi_frontend/tyrants/src/components/menuAndOutPut/preProcessInfo.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 import {Card} from 'react-native-shadow-cards';
-import {ScrollView, StyleSheet, Text, View, LogBox,  TouchableOpacity,} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  LogBox,
+  TouchableOpacity,
+} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -39,123 +46,115 @@ class Info extends Component {
             </View>
           </View>
           <TouchableOpacity
-              onPress={() =>
-                this.props.navigation.navigate('Pre-process', {
-                  imgUri: this.state.imgUri,
-                })
-              }>
-              <View>
-                <Close
-                  // source={require('../../images/icons/retake.svg')}
-                  style={{
-                    width: wp('7%'),
-                    height: hp('3%'),
-                    marginRight: 25,
-                    marginTop: 14,
-                  }}
-                />
-              </View>
-            </TouchableOpacity>
+            onPress={() =>
+              this.props.navigation.navigate('Pre-process', {
+                imgUri: this.state.imgUri,
+              })
+            }>
+            <View>
+              <Close
+                // source={require('../../images/icons/retake.svg')}
+                style={{
+                  width: wp('7%'),
+                  height: hp('3%'),
+                  marginRight: 25,
+                  marginTop: 14,
+                }}
+              />
+            </View>
+          </TouchableOpacity>
         </View>
         <ScrollView>
           <Card style={styles.card}>
             <View style={{}}>
-            <Grid>
-              <Row style={{height: hp("15%"), marginRight: wp("5%")}}>
-                <Col style={{width: wp("25%"), alignItems: 'center', justifyContent: 'center'}}>
-                <Threshold />
-                </Col>
-                <Col style={{justifyContent: 'center'}}>
-                <Text>
-                Reduces the photo to black and white; colors lighter than the
-                selectable threshold are converted to white,darker colors will
-                be black
-              </Text>
-                </Col>
-              </Row>
-            </Grid>
-              
-              
+              <Grid>
+              <Row style={styles.rowStyle}>
+                  <Col style={styles.colStyle}>
+                    <Threshold />
+                  </Col>
+                  <Col style={{justifyContent: 'center'}}>
+                    <Text>
+                      Reduces the photo to black and white; colors lighter than
+                      the selectable threshold are converted to white,darker
+                      colors will be black
+                    </Text>
+                  </Col>
+                </Row>
+              </Grid>
             </View>
           </Card>
 
           <Card style={styles.card}>
             <View style={{}}>
-            <Grid>
-              <Row style={{height: hp("15%"), marginRight: wp("5%")}}>
-                <Col style={{width: wp("25%"), alignItems: 'center', justifyContent: 'center'}}>
-                <Erosion />
-                </Col>
-                <Col style={{justifyContent: 'center'}}>
-                <Text>
-                It erodes away the boundaries of foreground object. It is useful
-                for removing smaill white noises detach two connected objects
-              </Text>
-                </Col>
-              </Row>
-            </Grid>
-              
-
-              
+              <Grid>
+              <Row style={styles.rowStyle}>
+                  <Col style={styles.colStyle}>
+                    <Erosion />
+                  </Col>
+                  <Col style={{justifyContent: 'center'}}>
+                    <Text>
+                      It erodes away the boundaries of foreground object. It is
+                      useful for removing smaill white noises detach two
+                      connected objects
+                    </Text>
+                  </Col>
+                </Row>
+              </Grid>
             </View>
           </Card>
 
           <Card style={styles.card}>
             <View>
-            <Grid>
-              <Row style={{height: hp("15%"), marginRight: wp("5%")}}>
-                <Col style={{width: wp("25%"), alignItems: 'center', justifyContent: 'center'}}>
-                <Dialation />
-                </Col>
-                <Col style={{justifyContent: 'center'}}>
-                <Text>
-                It is just opposite of erosion. It is also useful in joining
-                broken parts of an object
-              </Text>
-                </Col>
-              </Row>
-            </Grid>
-              
-
-              
+              <Grid>
+              <Row style={styles.rowStyle}>
+                  <Col style={styles.colStyle}>
+                    <Dialation />
+                  </Col>
+                  <Col style={{justifyContent: 'center'}}>
+                    <Text>
+                      It is just opposite of erosion. It is also useful in
+                      joining broken parts of an object
+                    </Text>
+                  </Col>
+                </Row>
+              </Grid>
             </View>
           </Card>
 
           <Card style={styles.card}>
             <View style={{}}>
-            <Grid>
-              <Row style={{height: hp("15%"), marginRight: wp("5%")}}>
-                <Col style={{width: wp("25%"), alignItems: 'center', justifyContent: 'center'}}>
-                <Opening />
-                </Col>
-                <Col style={{justifyContent: 'center'}}>
-                  <Text>
-                  It is obtained by the erosion of an image followed by a dilation. Useful for removing small objects
-                (it is assumed that the objects are bright on a dark foreground)
-                  </Text>
-                </Col>
-              </Row>
-            </Grid>
+              <Grid>
+                <Row style={styles.rowStyle}>
+                  <Col style={styles.colStyle}>
+                    <Opening />
+                  </Col>
+                  <Col style={{justifyContent: 'center'}}>
+                    <Text>
+                      It is obtained by the erosion of an image followed by a
+                      dilation. Useful for removing small objects (it is assumed
+                      that the objects are bright on a dark foreground)
+                    </Text>
+                  </Col>
+                </Row>
+              </Grid>
             </View>
           </Card>
 
           <Card style={styles.card}>
             <View style={{}}>
-            <Grid>
-              <Row style={{height: hp("15%"), marginRight: wp("5%")}}>
-                <Col style={{width: wp("25%"), alignItems: 'center', justifyContent: 'center'}}>
-                  <Closing />
-                </Col>
-                <Col style={{justifyContent: 'center'}}>
-                  <Text>
-                    It is obtained by the dilation of an image followed by an erosion. Useful to remove small holes (dark regions)
-                  </Text>
-                </Col>
-              </Row>
-            </Grid>
-              
-
-              
+              <Grid>
+                <Row style={styles.rowStyle}>
+                  <Col style={styles.colStyle}>
+                    <Closing />
+                  </Col>
+                  <Col style={{justifyContent: 'center'}}>
+                    <Text>
+                      It is obtained by the dilation of an image followed by an
+                      erosion. Useful to remove small holes (dark regions)
+                    </Text>
+                  </Col>
+                </Row>
+              </Grid>
             </View>
           </Card>
         </ScrollView>
@@ -165,21 +164,29 @@ class Info extends Component {
 }
 
 const styles = StyleSheet.create({
+  rowStyle: {height: hp('15%'), marginRight: wp('5%')},
+  colStyle: {
+    width: wp('25%'),
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     backgroundColor: '#333',
     height: '100%',
     paddingLeft: 3,
-    paddingRight: 3
+    paddingRight: 3,
   },
   text: {
+    color: '#ffffff',
     textAlign: 'justify',
   },
   card: {
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    // backgroundColor: 'rgba(186, 186, 186, 0.08)',
+    backgroundColor: 'rgba(186, 186, 186, 0.5)',
     width: 342,
     padding: 10,
     marginLeft: 20,
-    marginBottom: 20
+    marginBottom: 20,
   },
   textContainer: {
     marginTop: 20,
@@ -192,7 +199,7 @@ const styles = StyleSheet.create({
     color: '#FFC542',
     fontWeight: 'bold',
     fontFamily: 'SF Pro Rounded',
-    marginLeft: wp("5%")
+    marginLeft: wp('5%'),
   },
 });
 
